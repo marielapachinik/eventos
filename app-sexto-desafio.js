e3f2a5aad7adadee08cd95723737e208ab79a706
Mariela Pachinik <marielapachinikIT@gmail.com>
2022-09-14 23:51:05 -0300

    Creo un programa de inscripcion en formato autogestion

--- app-sexto-desafio.js
+++ app-sexto-desafio.js
@@ -1,4 +1,8 @@
 
+/* En éste desafio hice un programa de inscripcion en formato autogestión para que
+directamente se inscriban los alumnos */
+
+
 
 /* Funcion para convertir a entero.
 si era un entero no influye. Si no lo era, lo intenta convertir */
@@ -8,28 +12,42 @@ function validarEntero(valor){
 
     //Compruebo si es un valor numérico
     if (isNaN(valor)) {
-          //entonces (no es numero) devuelvo el valor cadena vacia
+          //entonces (no es numero) devuelvo un string vacío
           return ""
     }else{
-          //En caso contrario (Si era un número) devuelvo el valor
+          //En caso contrario, devuelvo el valor
           return valor
     }
 }
 
 
 
-//Funncion para validar los datos ingresados
+
+function validarEmail(valor) {
+       if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
+              return valor;
+       } else {
+        alert("La dirección de email es incorrecta!.");
+        return false;
+       }
+     }
+
+
+
+//Funcion principal para validar los datos ingresados
 
 function valida_envia(){
     
-    //valido el nombre
+    //valido el nombre. No debe quedar el campo vacio.
+    
     if (document.fvalida.nombre.value.length==0){
            alert("Tiene que escribir su nombre")
            document.fvalida.nombre.focus() // uso focus para hacer foco en el campo selecionado
            return 0;
     }
 
-    //Valido el apellido
+    //Valido el apellido. No debe quedar el campo vacio.
+    
     if (document.fvalida.apellido.value.length==0 ){
         alert("Tiene que escribir su apellido")
         document.fvalida.apellido.focus()
@@ -37,7 +55,8 @@ function valida_envia(){
        }
 
  
-    //valido el DNI. tiene que ser entero.
+    //valido el DNI. Tiene que ser un número entero.
+    
     dni = document.fvalida.dni.value
     dni = validarEntero(dni)
     document.fvalida.dni.value=dni
@@ -53,17 +72,38 @@ function valida_envia(){
               }
     }
 
+    // Valido el email.
+    
+    email = document.fvalida.email.value
+    email = validarEmail(email)
+    document.fvalida.email.value=email
+    if (email==""){
+           alert("Debe introducir un correo electronico valido.")
+           document.fvalida.email.focus()
+           return 0;
+    }else{
+           if (email==false){
+                  alert("El correo ingresado no es valido.")
+                  document.fvalida.email.focus()
+                  return 0;
+              }
+    }
+
+    
 
 
-    //validar el curso
+    //valido el curso
 
     if (document.fvalida.curso.selectedIndex==0){
            alert("Debe seleccionar un curso.")
            document.fvalida.curso.focus()
            return 0;
     }
+
+
  
     // Envío del formulario
-    alert("Muchas gracias por enviar el formulario");
+    
+    alert("Formulario completado con éxito \n Pronto recibirá un correo para confirmar la inscripción");
     document.fvalida.submit();
 }
